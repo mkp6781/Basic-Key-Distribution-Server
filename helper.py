8758cee501d8605a74eb0e20aa23c66a1060fdf5
Manu_K_Paul <mkp6781@gmail.com>
2021-04-09 01:28:49 +0530

    kdc/client/helper: Authentication of sender and creation of session key.
    This commit enables a user to authenticate himself to the KDC and obtain a
    session key for communicating securely with another user reigstered with the
    same KDC.
    Once the user is authenticated by the server, the server returns a message
    which contains the following data:
     * A message encrypted with requesting client's secret key containing session
       key and information regarding the reciever(ip address and port number).
     * Similar message encrypted with reciever's secret key containing information
       regarding sender(ip address and port number) and session key.

--- helper.py
+++ helper.py
@@ -1,21 +1,58 @@
 import base64
+from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import json
 import socket
 
-def send_message(data: dict, s: socket) -> str:
+
+def send_message(data: dict, s: socket):
     json_format_data = json.dumps(data).encode('utf-8')
     s.sendall(json_format_data)
-    response = s.recv(1024)
-    response = json.loads(response)
-    return response
 
 
 # encrypt plain_text using MD5 hash and store as user's secret key.
-def md5_hash(plain_text: str) -> str:
+def md5_hash(plain_text: str) -> bytes:
     digest = hashes.Hash(hashes.MD5())
     digest.update(bytes(plain_text, encoding='utf-8'))
     hashed_pass = digest.finalize()
     base64_hashed_pass_bytes = base64.b64encode(hashed_pass)
-    hashed_pass_string = base64_hashed_pass_bytes.decode("ascii")
-    return hashed_pass_string
+    return base64_hashed_pass_bytes
+
+
+def encrypt_msg(secret_key: bytes, msg_to_encrypt: dict) -> str:
+    f = Fernet(secret_key)
+    json_format_msg = json.dumps(msg_to_encrypt).encode('utf-8')
+    encrypted_msg = f.encrypt(json_format_msg)
+    encrypted_msg = encrypted_msg.decode("utf-8")
+    return encrypted_msg
+
+
+def derive_key(salt: bytes) -> bytes:
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(b"hello world"))
+    return key
+
+
+def get_user_info(OUTPUT_FILE: str, pwdfile: str, name: str) -> tuple:
+    # Get password hash of sender.
+    with open(OUTPUT_FILE / pwdfile, "r") as pwd:
+        lines = pwd.readlines()
+        for line in lines:
+            content = line.split(":")
+            if content[1] == name:
+                ip_addr_user = content[2]
+                portno_user = content[3]
+                salt_string = content[4]
+    return (ip_addr_user, portno_user, salt_string)
+
+
+def get_secret_key(salt_string: str) -> bytes:
+    salt_bytes = salt_string.encode("utf-8")
+    secret_key_sender = derive_key(salt_bytes)
+    return secret_key_sender
